791a6133ef7452933f01002746cb5113ddc21525
justu <dingzhongz1984@gmail.com>
2018-04-21 19:47:36 +0800

    添加接口测试功能

--- src/main/java/com/chris/modules/inf/controller/InterfaceTestController.java
+++ src/main/java/com/chris/modules/inf/controller/InterfaceTestController.java
@@ -1,5 +1,6 @@
 package com.chris.modules.inf.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.chris.common.utils.ResponseModel;
 import com.chris.common.utils.RestTemplateUtils;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author chris
@@ -24,8 +28,12 @@ public class InterfaceTestController {
      */
     @RequestMapping("/query")
 //    @RequiresPermissions(" res:resattachment:list")
-    public ResponseModel query(@RequestBody JSONObject param){
-        ResponseModel resp = restTemplateUtils.httpPostMediaTypeJson(restTemplateUtils.accessControlURL + "/passportStatistics/statisticsByCondition", ResponseModel.class, param);
+    public JSONObject query(@RequestBody JSONObject param){
+        String url = restTemplateUtils.accessControlURL + "/reformer/interface/remotecontrol/opendoor";
+//        JSONObject resp = restTemplateUtils.httpPostMediaTypeJson(url, JSONObject.class, param);
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("data", param.get("data"));
+        JSONObject resp = this.restTemplateUtils.httpGetUrlVariable(url, JSONObject.class, paramMap);
         return resp;
     }
 }
